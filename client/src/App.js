6ee54c7ae9f051a170659da949639e5fbbc102c6
AjayKumarSaini8 <108985901+AjayKumarSaini8@users.noreply.github.com>
2024-11-17 19:19:07 +0530

    product, transaction, geodata

--- client/src/App.js
+++ client/src/App.js
@@ -7,19 +7,27 @@ import { useSelector } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 import Layout from "scenes/layout";
 import Dashboard from "scenes/dashboard";
+import Products from "scenes/products";
+import Customers from "scenes/customers";
+import Transactions from "scenes/transactions";
+import Geography from "scenes/geography";
 
 function App() {
-  const mode = useSelector((state) => state.global.mode)
-  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="app">
       <BrowserRouter>
-        <ThemeProvider theme={theme} >
+        <ThemeProvider theme={theme}>
           <CssBaseline />
           <Routes>
             <Route element={<Layout />}>
               <Route path="/" element={<Navigate to="/dashboard" replace />} />
               <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/customers" element={<Customers />} />
+              <Route path="/transactions" element={<Transactions />} />
+              <Route path="/geography" element={<Geography />} />
             </Route>
           </Routes>
         </ThemeProvider>
